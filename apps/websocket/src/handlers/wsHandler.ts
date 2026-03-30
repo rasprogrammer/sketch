72fc8f58c3d59e98d39582d9d0801669750d8a81
Rajeev Kumar <rasprogrammer@gmail.com>
2026-03-30 21:08:05 +0530

    ws defined event handlers with empty method

--- apps/websocket/src/handlers/wsHandler.ts
+++ apps/websocket/src/handlers/wsHandler.ts
@@ -4,5 +4,25 @@ import type { WebSocketServer } from "ws";
 export const setupWebSocketServer = (wss: WebSocketServer) => {
     wss.on('connection', (socket, request) => {
         
-    })
+
+        // Handle incoming messages
+        socket.on('message', (data) => {
+            try {
+                const message = JSON.parse(data.toString());
+                
+            } catch (error) {
+
+            }
+        });
+
+        // Handle disconnetion 
+        socket.on('close', () => {
+
+        });
+
+    });
+
+    wss.on("listening", () => {
+
+    });
 }
